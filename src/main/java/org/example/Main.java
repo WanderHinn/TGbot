92587c5e1971d75794dc15f3812708861d626a69
Ander Hinn <Anderhinn@ut.ee>
2026-08-23 01:23:00 +0300

    All ready

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,7 +5,7 @@ import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
 
 public class Main {
-    static void main(String[] args) throws TelegramApiException {
+    public static void main(String[] args) throws TelegramApiException {
         TelegramBotsApi botsApi = new TelegramBotsApi(DefaultBotSession.class);
         botsApi.registerBot(new Client());
         System.out.println("Let's start!");
